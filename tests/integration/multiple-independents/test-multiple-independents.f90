201eb3a5c2df954fa6a478e0eb4e428d57aea6d4
Damian Rouson <damian@sourceryinstitute.org>
2020-04-09 16:09:25 -0700

    multiple-indendents test dependency matrix passes
    + white space adjustments

--- tests/integration/multiple-independents/test-multiple-independents.f90
+++ tests/integration/multiple-independents/test-multiple-independents.f90
@@ -1,80 +1,65 @@
-!*******************************************************************************
-!>
-!  DAG module test program.
+program multiple_independents
+  !!  Test topological sort and dependency matrix for a DAG with multiple independent nodes.
+  use iso_fortran_env, only : error_unit
+  use dag_interface, only : dag
+  implicit none
 
-    program multiple_independents
+  type(dag) :: d
 
-    use iso_fortran_env, only : error_unit
-    use dag_interface, only : dag
+  integer, allocatable :: order(:) !! topological sort
+  logical, allocatable :: mat(:,:) !! dependency matrix
 
-    implicit none
+  integer :: i, row, istat
+  integer,parameter :: n_nodes = 14, success=0
+  logical :: expected_mat(n_nodes,n_nodes) = .false.
+  integer, parameter :: expected_order(*) = [(i,i=1,n_nodes)]
+  character(len=*),parameter :: filetype = 'pdf'  !! filetype for output plot ('pdf', png', etc.)
+  character(len=*), parameter :: gray_square = 'shape=square,fillcolor="SlateGray1",style=filled'
+  character(len=len(gray_square)), parameter :: silk_circle = 'shape=circle,fillcolor="cornsilk",style=filled'
 
-    type(dag) :: d
+  call d%set_vertices(n_nodes)
+  call d%set_edges( 2,[integer::]) !2 depends on nothing
+  call d%set_edges( 3,[integer::]) !3 depends on nothing
+  call d%set_edges( 4,[3])  ; expected_mat(4,3)    = .true. !4 depends on  3
+  call d%set_edges( 5,[4])  ; expected_mat(5,4)    = .true. !5 depends on  4
+  call d%set_edges( 6,[3])  ; expected_mat(6,3)    = .true. !6 depends on  3
+  call d%set_edges( 7,[4])  ; expected_mat(7,4)    = .true. !2 depends on  4
+  call d%set_edges( 8,[5])  ; expected_mat(8,5)    = .true. !3 depends on  5
+  call d%set_edges( 9,[6])  ; expected_mat(9,6)    = .true. !4 depends on  6
+  call d%set_edges(10,[6,7]); expected_mat(10,6:7) = .true. !5 depends on  6 and 7
+  call d%set_edges(11,[7,8]); expected_mat(11,7:8) = .true. !6 depends on  7 and 8
+  call d%set_edges(12,[9])  ; expected_mat(12,9)   = .true. !2 depends on  9
+  call d%set_edges(13,[10]) ; expected_mat(13,10)  = .true. !3 depends on 10
+  call d%set_edges(14,[11]) ; expected_mat(14,11)  = .true. !4 depends on 11
 
-    integer, allocatable :: order(:) !! topological sort
-    logical, allocatable :: mat(:,:) !! dependency matrix
+  call d%toposort(order,istat)
 
-!    logical, parameter :: expected_mat(*,*) = reshape( [ &
-!      .false., .true. , .true. , .false., .false., .false., &
-!      .false., .false., .false., .false., .true. , .true. , &
-!      .false., .false., .false., .false., .false., .false., &
-!      .false., .false., .false., .false., .false., .true. , &
-!      .false., .false., .true. , .true. , .false., .false., &
-!      .false., .false., .false., .false., .false., .false. &
-!      ], [6,6])
-      !! expected dependency matrix
-    integer :: i, row, istat
-    integer,parameter :: n_nodes = 14, success=0
-    integer, parameter :: expected_order(*) = [(i,i=1,n_nodes)]
-!    character(len=*),parameter :: filetype = 'pdf'  !! filetype for output plot ('pdf', png', etc.)
-!    character(len=*), parameter :: gray_square = 'shape=square,fillcolor="SlateGray1",style=filled'
-!    character(len=len(gray_square)), parameter :: silk_circle = 'shape=circle,fillcolor="cornsilk",style=filled'
+  if (istat/=success) then
+    write(error_unit, *) 'istat =', istat
+    error stop
+  end if
 
-    call d%set_vertices(n_nodes)
-    call d%set_edges( 2,[integer::])    !2 depends on  0?
-    call d%set_edges( 3,[integer::])    !3 depends on  0?
-    call d%set_edges( 4,[3])   !4 depends on  3
-    call d%set_edges( 5,[4])   !5 depends on  4
-    call d%set_edges( 6,[3])   !6 depends on  3
-    call d%set_edges( 7,[4])   !2 depends on  4
-    call d%set_edges( 8,[5])   !3 depends on  5
-    call d%set_edges( 9,[6])   !4 depends on  6
-    call d%set_edges(10,[6,7]) !5 depends on  6 and 7
-    call d%set_edges(11,[7,8]) !6 depends on  7 and 8
-    call d%set_edges(12,[9])   !2 depends on  9
-    call d%set_edges(13,[10])  !3 depends on 10
-    call d%set_edges(14,[11])  !4 depends on 11
+  if (any(order /= expected_order)) then
+    write(error_unit, *) 'order =', order
+    error stop
+  end if
 
-    call d%toposort(order,istat)
+  do i = 1, n_nodes
+    call d%set_vertex_info(i, attributes = merge(gray_square, silk_circle, any(i==[1,2,12,13,14])))
+  end do
+  call d%save_digraph('test2.dot','RL',300) ! TODO: verify internal generate_diagraph result against expected result
+  call execute_command_line('dot -T'//filetype//' -o test2.'//filetype//' test2.dot')
 
-    if (istat/=success) then
-      write(error_unit, *) 'istat =', istat
-      error stop
-    end if
+  call d%generate_dependency_matrix(mat)
 
-print *, order
+  if (any(mat .neqv. expected_mat)) then
+    write(error_unit,*) 'main: incorrect dependency matrix:'
+    do row = 1, n_nodes
+      write(error_unit,*) merge('X', 'O', mat(row,:)) ! write row
+    end do
+    error stop
+  end if
 
-    if (any(order /= expected_order)) then
-      write(error_unit, *) 'order =', order
-      error stop
-    end if
+  print *,"Test passed."
 
-!    do i = 1, n_nodes
-!      call d%set_vertex_info(i, attributes = merge(gray_square, silk_circle, any(i==[3,6])))
-!    end do
-!    call d%save_digraph('test.dot','RL',300) ! TODO: verify internal generate_diagraph result against expected result
-!    call execute_command_line('dot -T'//filetype//' -o test.'//filetype//' test.dot')
-
-!    call d%generate_dependency_matrix(mat)
-!    if (any(mat .neqv. expected_mat)) then
-!      write(error_unit,*) 'main: incorrect dependency matrix:'
-!      do row = 1, n_nodes
-!        write(error_unit,*) merge('X', 'O', mat(row,:)) ! write column
-!      end do
-!      error stop
-!    end if
-
-    print *,"Test passed."
-
-  end program multiple_independents
-!*******************************************************************************
+end program multiple_independents
