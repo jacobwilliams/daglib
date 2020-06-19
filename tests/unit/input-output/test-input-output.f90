1c43a4f1ca765aac719a74a1c60607ca84c6ead2
Damian Rouson <damian@sourceryinstitute.org>
2020-06-19 01:26:17 -0700

    finish test of dag input in JSON format

--- tests/unit/input-output/test-input-output.f90
+++ tests/unit/input-output/test-input-output.f90
@@ -7,6 +7,10 @@ program dag_input
 
   call dependency_graph%input("dag-vertices-edges.json")
 
+  if ( any(dependency_graph%get_edges(1) /= [2,3]      )) error stop "dag%get_edges(1) incorrect"
+  if ( any(dependency_graph%get_edges(2) /= [3]        )) error stop "dag%get_edges(2) incorrect"
+  if ( any(dependency_graph%get_edges(3) /= [integer::])) error stop "dag%get_edges(3) incorrect"
+
   sync all
   if (this_image()==1) print *,"Test passed."
 
