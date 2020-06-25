61e9d8264e460c60032d3b2c28ebfe8d1a665baa
Damian Rouson <damian@sourceryinstitute.org>
2020-06-25 10:25:15 -0700

    add derived type output to test-input-output

--- tests/unit/input-output/test-input-output.f90
+++ tests/unit/input-output/test-input-output.f90
@@ -1,6 +1,7 @@
 program dag_input
   !!  Test DAG input/output.
   use dag_interface, only : dag
+  use assert_interface, only : assert
   implicit none
 
   type(dag) :: dependency_graph
@@ -10,6 +11,18 @@ program dag_input
   if ( any(dependency_graph%get_edges(1) /= [2,3]      )) error stop "dag%get_edges(1) incorrect"
   if ( any(dependency_graph%get_edges(2) /= [3]        )) error stop "dag%get_edges(2) incorrect"
   if ( any(dependency_graph%get_edges(3) /= [integer::])) error stop "dag%get_edges(3) incorrect"
+
+  block
+    integer file_unit, io_status
+    integer, parameter :: success = 0
+    character(len=128) :: error_message
+
+    open(newunit=file_unit, file="dag-output.json", status="unknown", iostat=io_status, iomsg=error_message)
+    call assert(io_status==success, "io_status==0", error_message)
+
+    write(file_unit,*) dependency_graph
+    close(file_unit)
+  end block
 
   sync all
   if (this_image()==1) print *,"Test passed."
