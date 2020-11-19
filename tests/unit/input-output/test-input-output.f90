903eda0fc1db9f6e3c33af94ee930a6a111470d8
Damian Rouson <damian@sourceryinstitute.org>
2020-11-19 09:51:51 -0800

    test(I/O): add I/O unit test supporting function

--- tests/unit/input-output/test-input-output.f90
+++ tests/unit/input-output/test-input-output.f90
@@ -1,33 +1,40 @@
-program dag_input_output
+module dag_input_output_test
   !!  Test DAG input/output.
-  use dag_interface, only : dag
-  use assert_interface, only : assert
   implicit none
 
-  type(dag) :: dependency_graph
-  integer, parameter :: n_nodes = 6, success=0
-  integer istat
+contains
 
-  call dependency_graph%set_vertices(n_nodes)
-  call dependency_graph%set_edges(2,[1])     !2 depends on 1
-  call dependency_graph%set_edges(3,[5,1])   !3 depends on 5 and 1
-  call dependency_graph%set_edges(4,[5])     !4 depends on 5
-  call dependency_graph%set_edges(5,[2])     !5 depends on 2
-  call dependency_graph%set_edges(6,[2,4])   !6 depends on 2 and 4
+  function check_input_output() result(io_result)
+    use dag_interface, only : dag
+    use vegetables, only : result_t, succeed
 
-  block
-    integer file_unit, io_status
-    integer, parameter :: success = 0
-    character(len=128) :: error_message
+    type(result_t) io_result
+    type(dag) :: dependency_graph
+    integer, parameter :: n_nodes = 6, success=0
+    integer istat
 
-    open(newunit=file_unit, file="output/dag-output.json", status="unknown", iostat=io_status, iomsg=error_message)
-    call assert(io_status==success, "io_status==0", error_message)
+    call dependency_graph%set_vertices(n_nodes)
+    call dependency_graph%set_edges(2,[1])     !2 depends on 1
+    call dependency_graph%set_edges(3,[5,1])   !3 depends on 5 and 1
+    call dependency_graph%set_edges(4,[5])     !4 depends on 5
+    call dependency_graph%set_edges(5,[2])     !5 depends on 2
+    call dependency_graph%set_edges(6,[2,4])   !6 depends on 2 and 4
 
-    write(file_unit,*) dependency_graph
-    close(file_unit)
-  end block
+    block
+      integer file_unit, io_status
+      integer, parameter :: success = 0
+      character(len=128) :: error_message
 
-  sync all
-  if (this_image()==1) print *,"Test passed: input-output"
+      if (this_image()==1) then
 
-end program dag_input_output
+        open(newunit=file_unit, file="output/dag-output.json", status="unknown", iostat=io_status, iomsg=error_message)
+        io_result = asertEquals(io_status,0)
+
+        write(file_unit,*) dependency_graph
+        close(file_unit)
+      end if
+    end block
+
+  end function
+
+end module dag_input_output_test
